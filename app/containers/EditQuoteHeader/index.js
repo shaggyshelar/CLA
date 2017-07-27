c87e21a24e3f3c180960b8020ef5377944cd69db
manishpatil159 <manish.patil159@gmail.com>
2017-07-27 11:33:27 +0530

    moved onload calls to edit quote page

--- app/containers/EditQuoteHeader/index.js
+++ app/containers/EditQuoteHeader/index.js
@@ -8,68 +8,77 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Helmet from 'react-helmet';
 import { createStructuredSelector } from 'reselect';
-// import { makeSelectData, makeSelectError, makeSelectLoading } from './selectors';
+// import { makeSelectData, makeSelectError, makeSelectLoading } from
+// './selectors';
 import AddProductsDropdown from '../../components/AddProductsDropdown';
 import AddGroupDropdown from '../../components/AddGroupDropdown';
 import EditQuoteHeaderCard from '../../components/EditQuoteHeaderCard';
 import { Button, Glyphicon, Row, Col, ButtonGroup } from 'react-bootstrap/lib';
 
-
 export class EditQuoteHeader extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
-constructor(props) {
-    super(props)
-    this.handleFullScreen = this.handleFullScreen.bind(this)
-}
- 
+  constructor(props) {
+    super(props);
+    this.handleFullScreen = this
+      .handleFullScreen
+      .bind(this);
+  }
+
   componentDidMount() {
-    this.props.getOnLoadData();
+    // this.props.getOnLoadData();
     const w = window,
-  d = document,
-  documentElement = d.documentElement;
-  console.log(documentElement)
-   this.handleFullScreen(documentElement);
+      d = document,
+      documentElement = d.documentElement;
+    console.log(documentElement);
+    this.handleFullScreen(documentElement);
   }
- handleFullScreen(d) {
-  if(d.requestFullscreen) {
-    d.requestFullscreen();
-  } else if(d.mozRequestFullScreen) {
-    d.mozRequestFullScreen();
-  } else if(d.webkitRequestFullscreen) {
-    d.webkitRequestFullscreen();
-  } else if(document.documentElement.msRequestFullscreen) {
-    d.msRequestFullscreen();
+  handleFullScreen(d) {
+    if (d.requestFullscreen) {
+      d.requestFullscreen();
+    } else if (d.mozRequestFullScreen) {
+      d.mozRequestFullScreen();
+    } else if (d.webkitRequestFullscreen) {
+      d.webkitRequestFullscreen();
+    } else if (document.documentElement.msRequestFullscreen) {
+      d.msRequestFullscreen();
+    }
   }
-}
   render() {
     const { loading, error, data } = this.props;
-    const dataListProps = {
-      loading,
-      error,
-      data,
-    };
+    // const dataListProps = {
+    //   loading,
+    //   error,
+    //   data,
+    // };
 
     return (
       <Row className="show-grid">
         <Col xs={12} md={3}>
           <Helmet
             title="TableHeader"
-            meta={[
-              { name: 'description', content: 'Description of TableHeader' },
+            meta={[{
+              name: 'description',
+              content: 'Description of TableHeader'
+            },
             ]}
           />
           <EditQuoteHeaderCard />
         </Col>
-        <Col xs={12} md={9} style={{textAlign:"right"}}>
+        <Col xs={12} md={9} style={{
+            textAlign: "right",
+        }}
+        >
           <AddProductsDropdown />
           <AddGroupDropdown />
           <Button className="margin">Delete Lines</Button>
-           <ButtonGroup className="margin">
-          <Button>Quick Save</Button>
-          <Button>Calculate</Button>
-           </ButtonGroup>
+          <ButtonGroup className="margin">
+            <Button>Quick Save</Button>
+            <Button>Calculate</Button>
+          </ButtonGroup>
           <Button className="margin">Cancel</Button>
-          <Button className="margin" onClick={this.handleFullScreen}><Glyphicon glyph='fullscreen' /></Button>
-          <Button onClick={() => this.props.getOnLoadData()} className="margin" bsStyle="primary">Save</Button>
+          <Button className="margin" onClick={this.handleFullScreen}><Glyphicon glyph='fullscreen'/></Button>
+          <Button
+            className="margin"
+            bsStyle="primary">Save</Button>
         </Col>
       </Row>
     );
@@ -78,23 +87,20 @@ constructor(props) {
 
 EditQuoteHeader.propTypes = {
   loading: React.PropTypes.bool,
-  error: React.PropTypes.oneOfType([
-    React.PropTypes.object,
-    React.PropTypes.bool,
-  ]),
+  error: React
+    .PropTypes
+    .oneOfType([React.PropTypes.object, React.PropTypes.bool]),
   data: React.PropTypes.object,
   //getAllData: React.PropTypes.func,
-  getOnLoadData: React.PropTypes.func,
+  // getOnLoadData: React.PropTypes.func
 };
 
-const mapStateToProps = createStructuredSelector({
-
-});
+const mapStateToProps = createStructuredSelector({});
 
 function mapDispatchToProps(dispatch) {
 
   return {
-    // dispatch,   
+    // dispatch,
   };
 }
 
--- app/containers/EditQuotePage/index.js
+++ app/containers/EditQuotePage/index.js
@@ -13,18 +13,33 @@ import { EditQuoteHeader } from '../EditQuoteHeader';
 import EditQuoteGrid from 'components/EditQuoteGrid';
 import { loadData } from './actions';
 
-
 export class EditQuotePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+  componentDidMount() {
+    this
+      .props
+      .getAllData();
+  }
+
   render() {
+    const { loading, error, data } = this.props;
+    const dataListProps = {
+      loading,
+      error,
+      data,
+    };
+
     return (
       <div>
         <Helmet
           title="EditQuotePage"
-          meta={[
-            { name: 'description', content: 'Description of EditQuotePage' },
+          meta={[{
+            name: 'description',
+            content: 'Description of EditQuotePage',
+          },
           ]}
         />
-        <EditQuoteHeader getOnLoadData={this.props.getAllData} />
+        {/* <EditQuoteHeader getOnLoadData={this.props.getAllData} /> */}
+        <EditQuoteHeader data={this.props.data} />
         <EditQuoteGrid data={this.props.data} />
       </div>
     );
@@ -34,6 +49,12 @@ export class EditQuotePage extends React.PureComponent { // eslint-disable-line 
 EditQuotePage.propTypes = {
   dispatch: PropTypes.func.isRequired,
   getAllData: React.PropTypes.func,
+  loading: React.PropTypes.bool,
+  error: React
+    .PropTypes
+    .oneOfType([React.PropTypes.object, React.PropTypes.bool]),
+  data: React.PropTypes.object,
+  // getAllData: React.PropTypes.func,
 };
 
 const mapStateToProps = createStructuredSelector({
