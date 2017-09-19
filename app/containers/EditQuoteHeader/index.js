62e6850c08e350d9eba3f71c80c4ab13d929f426
harshmatic <harshit.jyoti@eternussolutions.com>
2017-09-19 14:44:23 +0530

    Css Changes

--- app/containers/EditQuoteHeader/index.js
+++ app/containers/EditQuoteHeader/index.js
@@ -7,6 +7,7 @@
 import React from 'react';
 import screenfull from 'screenfull';
 import Helmet from 'react-helmet';
+import _ from 'lodash';
 import { browserHistory } from 'react-router';
 import { Button, Row, Col, ButtonGroup } from 'react-bootstrap/lib';
 import { FormattedMessage, injectIntl } from 'react-intl';
@@ -45,7 +46,7 @@ export class EditQuoteHeader extends React.Component { // eslint-disable-line re
             :
             <Button title={this.context.intl.formatMessage({ ...messages.addProducts })} className="margin" onClick={() => { browserHistory.push(`/ProductSelection?PriceBookId=${this.props.data.priceBookId}`); }}><FormattedMessage {...messages.addProducts} /></Button>
           }
-          {(!this.props.grouped && this.props.data.groups.length === 0) ?
+          {(!this.props.grouped && _.filter(this.props.data.groups, { isDeleted: false }).length === 0) ?
             <Button title={this.context.intl.formatMessage({ ...messages.addGroup })} className="margin" onClick={this.props.group}><FormattedMessage {...messages.addGroup} /></Button>
             :
             <ButtonGroup className="margin">
