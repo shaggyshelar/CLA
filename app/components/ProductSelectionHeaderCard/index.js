af8224e6694b0e84a79ac810bde160eeae12fc6e
harshmatic <harshit.jyoti@eternussolutions.com>
2017-10-04 14:56:55 +0530

    Remove icons from headers

--- app/components/ProductSelectionHeaderCard/index.js
+++ app/components/ProductSelectionHeaderCard/index.js
@@ -6,16 +6,16 @@
 
 import React from 'react';
 // import styled from 'styled-components';
-import { Glyphicon } from 'react-bootstrap/lib';
+// import { Glyphicon } from 'react-bootstrap/lib';
 
 class ProductSelectionHeaderCard extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
       <div className="card" >
-        <div className="card-icon"><Glyphicon className="cartIcon" glyph="barcode" /></div>
+        {/* <div className="card-icon"><Glyphicon className="cartIcon" glyph="barcode" /></div> */}
         <div className="card-detail">
           <div>
-            <div className="cartFont" style={{ marginTop: '5px' }}>Q-00116</div>
+            <div className="cartFont" >Add Products</div>
           </div>
         </div>
       </div>
--- app/components/ReconfigureProductHeaderCard/index.js
+++ app/components/ReconfigureProductHeaderCard/index.js
@@ -6,17 +6,17 @@
 
 import React from 'react';
 // import styled from 'styled-components';
-import { Glyphicon } from 'react-bootstrap/lib';
+// import { Glyphicon } from 'react-bootstrap/lib';
 import messages from './messages';
 
 class ReconfigureProductHeaderCard extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
       <div className="card" >
-        <div className="card-icon"><Glyphicon className="cartIcon" glyph="wrench" /></div>
+        {/* <div className="card-icon"><Glyphicon className="cartIcon" glyph="wrench" /></div> */}
         <div className="card-detail">
           <div>
-            <div className="cartFont">{this.props.quoteName} - <span style={{ marginTop: '0px', fontSize: '20px' }}>{this.context.intl.formatMessage({ ...messages.configureProducts })}</span></div>
+            <div className="cartFont"><span style={{ marginTop: '0px', fontSize: '20px' }}>{this.context.intl.formatMessage({ ...messages.configureProducts })}</span></div>
           </div>
         </div>
       </div>
