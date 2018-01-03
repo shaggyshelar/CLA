30bb526e35408b158b207a71a5af9bd9b7a07c58
Sagar Shelar <shaggy.shelar@gmail.com>
2018-01-03 17:08:12 +0530

    Added tabs for guided selling modal

--- app/components/GuidedSellingModal/index.js
+++ app/components/GuidedSellingModal/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Modal, Button, Glyphicon } from 'react-bootstrap/lib';
+import { Modal, Button, Glyphicon, Tab, Row, Col, Nav, NavItem } from 'react-bootstrap/lib';
 
 class GuidedSellingModal extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
@@ -12,9 +12,43 @@ class GuidedSellingModal extends React.Component { // eslint-disable-line react/
           <Modal.Header closeButton>
             <Modal.Title style={{ textAlign: 'center' }}> <Glyphicon glyph="calendar" /> <strong> Guided Selling </strong></Modal.Title>
           </Modal.Header>
-
           <Modal.Body>
-            <br /> Hello World!
+            <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+              <Row className="clearfix">
+                <Col sm={2}>
+                  <Nav bsStyle="pills" stacked>
+                    <NavItem eventKey="first">
+                      Category 1
+                    </NavItem>
+                    <NavItem eventKey="second">
+                      Category 2
+                    </NavItem>
+                    <NavItem eventKey="third">
+                      Category 3
+                    </NavItem>
+                    <NavItem eventKey="forth">
+                      Category 4
+                    </NavItem>
+                  </Nav>
+                </Col>
+                <Col sm={10}>
+                  <Tab.Content animation>
+                    <Tab.Pane eventKey="first">
+                    11 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="second">
+                    22 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="third">
+                    33 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="forth">
+                    4 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Col>
+              </Row>
+            </Tab.Container>
           </Modal.Body>
 
           <Modal.Footer>
