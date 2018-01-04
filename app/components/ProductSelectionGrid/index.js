92015c787fe9b96b30ec2d55cef6e565f947d038
Dakshineshwar Swain <dakshineshwar.swain@Eternus.local>
2018-01-04 12:15:09 +0530

    Implemented modal on product name click and resolved css issues

--- app/components/ProductSelectionGrid/index.js
+++ app/components/ProductSelectionGrid/index.js
@@ -1,6 +1,7 @@
 import ReactTable from '../ReactTable';
 import React from 'react';
 import { Glyphicon } from 'react-bootstrap/lib';
+import ProductDetails from '../ProductDetails';
 
 import messages from './messages';
 class ProductSelectionGrid extends React.Component { // eslint-disable-line react/prefer-stateless-function
@@ -21,21 +22,17 @@ class ProductSelectionGrid extends React.Component { // eslint-disable-line reac
         pivot: true,
         expander: true,
         freezeWhenExpanded: true,
-
+        selectedLine: {},
+        detailedInfo: {},
       },
       isVisible: false,
+      isProductDetailsModalOpen: false,
     };
+    this.handleProductDetailsToggle = this.handleProductDetailsToggle.bind(this);
+    this.renderOverlay = this.renderOverlay.bind(this);
     this.setTableOption = this.setTableOption.bind(this);
   }
-  renderActionItems(cellInfo) {
-    const notification = cellInfo.original.notificationMessages.length > 0 ? <a title={cellInfo.original.notificationMessages.map(item => item + "\n" )  } className={cellInfo.original.notificationMessages.length > 0 ? 'link' : 'disabled-link'}><Glyphicon glyph="bell" /></a>:'';
-    return (
-      <div className="actionItems" >
-        {notification}
-      </div>
-    );
-  }
-  
+
   setTableOption(event) {
     const target = event.target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
@@ -46,6 +43,36 @@ class ProductSelectionGrid extends React.Component { // eslint-disable-line reac
         [name]: value,
       },
     });
+  }
+
+  handleProductDetailsToggle(index) {
+    const selectedData = this.props.products[index];
+    if (selectedData !== undefined) {
+      this.setState({
+        isProductDetailsModalOpen: !this.state.isProductDetailsModalOpen,
+        detailedInfo: selectedData.detailedInfo,
+      });
+    } else {
+      this.setState({
+        isProductDetailsModalOpen: !this.state.isProductDetailsModalOpen,
+      });
+    }
+  }
+
+  renderActionItems(cellInfo) {
+    const notification = cellInfo.original.notificationMessages.length > 0 ? <a title={cellInfo.original.notificationMessages.map((item) => `${item }\n`)} className={cellInfo.original.notificationMessages.length > 0 ? 'link' : 'disabled-link'}><Glyphicon glyph="bell" /></a> : '';
+    return (
+      <div className="actionItems" >
+        {notification}
+      </div>
+    );
+  }
+
+
+  renderOverlay(cellInfo) {
+    return (
+      <div className="lab"><a onClick={this.handleProductDetailsToggle.bind(this, cellInfo.index)} title={`${cellInfo.original.name}`}>{cellInfo.original.name}</a> </div>
+    );
   }
   render() {
     const columns = [
@@ -81,6 +108,7 @@ class ProductSelectionGrid extends React.Component { // eslint-disable-line reac
         width: 250,
         style: { textAlign: 'left' },
         headerStyle: { textAlign: 'left' },
+        Cell: this.renderOverlay,
       },
       {
         Header: () => <span className="upper-case" title={this.context.intl.formatMessage({ ...messages.productFamily })}>{this.context.intl.formatMessage({ ...messages.productFamily })}</span>,
@@ -119,6 +147,14 @@ class ProductSelectionGrid extends React.Component { // eslint-disable-line reac
 
           />
         </div>
+        <ProductDetails
+          show={this.state.isProductDetailsModalOpen} onHide={this.handleProductDetailsToggle}
+          style={{
+            display: 'inline-flex',
+          }}
+          value={this.state.value}
+          detailedInfo={this.state.detailedInfo}
+        />
       </div>
     );
   }
