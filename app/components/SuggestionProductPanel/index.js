bd302c1b36bc31a42cd0fdec0547673221e33e71
Ajit More <Ajit More@ESPLD048.Eternus.local>
2018-04-09 11:08:54 +0530

    Bug fixing for suggestion model

--- app/components/SuggestionProductPanel/index.js
+++ app/components/SuggestionProductPanel/index.js
@@ -40,7 +40,7 @@ class SuggestionProductPanel extends React.Component { // eslint-disable-line re
             <span className="categoryLabel">{name}</span>
           </div>
           <div className="panel-group" id="suggestionAccordion">
-            <Panel id="panelCrossSell" defaultExpanded className="productAccordian">
+            <Panel id="panelCrossSell" className="productAccordian">
               <Panel.Heading className="panel-heading">              
                 <Panel.Title toggle>
                   CrossSell <Glyphicon glyph="chevron-down" />
